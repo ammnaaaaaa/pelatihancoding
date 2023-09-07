213bc9a0c0a10d6b2dc4dda8dfa8c460d6a226b4
ammnaaaaaa <143860061+ammnaaaaaa@users.noreply.github.com>
2023-09-07 18:13:40 +0700

    Add files via upload

---
+++ volume balok.py
@@ -0,0 +1,8 @@
+print("Program menghitung volume balok")
+p = int(input("masukan panjang balok"))
+l = int(input("masukan lebar balok"))
+t = int(input("masukan tinggi balok"))
+
+volume = p*l*t
+print ('volume balok adalah', volume)
+    
---
+++ volume tabung.py
@@ -0,0 +1,7 @@
+print("menghitung volume tabung")
+pi = 22/7
+r = int(input("masukkan jari jari"))
+t = int(input("masukkan tinggi"))
+
+volume = pi*r**2*t
+print ('volume tabung adalah', volume)
